7046720c9a31af8135c068e3e5ea76cbf0a0198d
Meseret <mesiteklu2018@gmail.com>
2023-11-09 23:37:32 -0700

    added new codes

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render( 
+<div>
+  <h1  className ="heading">My favourite Movies!</h1> 
+  <ul> 
+    <li>Friends</li> 
+    <li>Summer Vacation </li>
+    <li> Tom And Jerry </li> 
+   </ul>
+</div>
+     , document.getElementById("root"));
+     
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 li>st elements.
